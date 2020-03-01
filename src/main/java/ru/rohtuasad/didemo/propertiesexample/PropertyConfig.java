128435a8d8e6bcef969708817043c395881db1a9
Alexey <rohtuasad@gmail.com>
2020-03-01 13:12:41 +0300

    Read property from environment example

--- src/main/java/ru/rohtuasad/didemo/propertiesexample/PropertyConfig.java
+++ src/main/java/ru/rohtuasad/didemo/propertiesexample/PropertyConfig.java
@@ -1,14 +1,19 @@
 package ru.rohtuasad.didemo.propertiesexample;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
+    @Autowired
+    Environment env;
+
     @Value("${database.username}")
     String user;
 
@@ -21,7 +26,7 @@ public class PropertyConfig {
     @Bean
     public FakeDataSource fakeDataSource() {
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUser(user);
+        fakeDataSource.setUser(env.getProperty("USERNAME"));
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
         return fakeDataSource;
